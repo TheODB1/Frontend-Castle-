af73c7b6b0739d5b45a592438c17389e4c0c4545
ZhiJuanTang <tangzhijuan56@gmail.com>
2022-01-18 15:31:44 +0100

    added Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,7 @@ const Footer = () => {
     return (
       <div>
         <div className="textFooter">
-          <h5>Created by: Christ & Zhi.    Share or follow us on:</h5>
-          <h6></h6>
+          <h5>Created by: Christ & Zhi.  Share or follow us on:</h5>          
         </div>
         <div className="socialMedia">
           <a href="https://github.com/" target="_blank"  rel="noreferrer">
@@ -14,6 +13,9 @@ const Footer = () => {
           </a>
           <a href="https://www.linkedin.com/" target="_blank"  rel="noreferrer">
             <i className="fa fa-linkedin-square"></i>
+          </a>          
+          <a href="https://www.twitter.com/" target="_blank"  rel="noreferrer">
+            <i className="fa fa-twitter-square" ></i>
           </a>
           <a href="https://www.instagram.com/" target="_blank"  rel="noreferrer">
             <i className="fa fa-instagram" ></i>

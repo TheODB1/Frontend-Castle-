2e43bdede3ece02b82a384ecaafbccc714f1b3ab
ZhiJuanTang <tangzhijuan56@gmail.com>
2022-01-19 13:21:35 +0100

    added text-editor, log-in, register,protectedrpoute,userprofile component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,51 @@
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import AuthState from './context/AuthContext';
 import Footer from "./components/Footer";
 import CastleScreen from "./screens/CastleScreen";
 import HomeScreen from "./screens/HomeScreen";
 import LogIn from "./components/LogIn";
 import Register from "./components/Register";
+import Navbar from "./components/Navbar";
+import CreatePost from "./components/CreatePost";
+import Layout from "./components/Layout";
+import ProtectedRoute from "./components/ProtectedRoute";
+import UserProfile from "./components/UserProfile";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
+    <AuthState>
+    <ToastContainer />
     <Router>
       <div className="grid-container">
-        <header className="row navbar">
-          <Link to="/"> Home </Link>
-          <div>
-            <a className="logo" href="/">
-              <img src="/images/amatira(6).png" alt="Logo" width={500} />
-            </a>
-          </div>
-          <div>
-            <a href="/cart">Cart</a>
-            <Link to="/register"> Register </Link>
-            <Link to="/login"> SignIn </Link>
-          </div>
+        <header>
+          <Navbar />
         </header>
         <main>
           <Routes>
-            <Route path="/" element={<HomeScreen />} exact />
-            <Route path="login" element={<LogIn />}/>
-            <Route path="register" element={<Register />}/>
-            <Route path="castle">
-              <Route path=":castleId" element={<CastleScreen />} />
+            <Route path="/" element={<Layout />}>
+              <Route index element={<HomeScreen />} exact />
+              <Route path="/" element={<HomeScreen />} exact />
+              <Route path="login" element={<LogIn />} />
+              <Route path="register" element={<Register />} />
+              <Route path="castle/:castleId" element={<CastleScreen />} />
+              <Route path="create-post" element={<CreatePost />} />
+              <Route path="protected" element={<ProtectedRoute />}>
+                <Route index element={<UserProfile />} />
+                <Route path="create-post" element={<CreatePost />} />
+              </Route>
+              <Route path="*" element={<NotFound />} />
             </Route>
           </Routes>
-          <footer className="row center  footerMainDiv ">
-            <Footer />
-          </footer>
         </main>
+        <footer>
+          <Footer />
+        </footer>
       </div>
     </Router>
+  
+    </AuthState>
   );
 }
 export default App;
--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,44 +1,59 @@
 /*import { getSpaceUntilMaxLength } from '@testing-library/user-event/dist/utils';*/
 import React from "react";
-import ".//LogIn.css";
-
-/*
-let box = new gsap.timeline();
-
-box.form('.container', { width: '0%', stagger: 0.4, duration: 1.5});
-box.form('.from__title', { opacity: 0, y: -30 });
-box.form('.form__input-group input', { opacity: 0, stagger: 0.3 });
-
-box.form('.form__button', {opacity:0, y:20 });
-*/
+import "./LogIn.css";
+import { useForm } from 'react-hook-form';
+import { useLocation, Link, Navigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
 const LogIn = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || '/protected';
+
+  const { isAuthenticated, loading, signin } = useAuth();
+
+  const onSubmit = formData => signin(formData);
+  if (loading) return 'Loading...';
+  if (isAuthenticated) return <Navigate to={from} replace />;
   return (
-    <div className="container">
-      <div className="form" id="login">
-        <h1 className="from__title">SignIn</h1>
-        <div className="from__input-group">
-          <input
-            type={"text"}
-            className="from__input"
-            autoFocus
-            placeholder="Username"
-            id="usernameInput"
-          ></input>
-        </div>
-        <div className="from__input-group">
-          <input
-            type={"password"}
-            className="from__input"
-            autoFocus
-            placeholder="Password"
-            id="passwordInput"
-          ></input>
-          <div className="padding"></div>
-          <button className="from__button" id="login">
-            SignIn
+    <div className='row mt-5 justify-content-center'>
+      <div className='col-md-4'>
+        <form onSubmit={handleSubmit(onSubmit)} autoComplete='off'>
+          <div>
+            <label htmlFor='email' className='form-label'>
+              Email:
+            </label>
+            <input
+              className={errors.email ? 'form-control is-invalid' : 'form-control'}
+              type='email'
+              {...register('email', { required: true })}
+            />
+            {errors.email && <div className='invalid-feedback'>Email is required</div>}
+          </div>
+          <div>
+            <label htmlFor='password' className='form-label'>
+              Password:
+            </label>
+            <input
+              className={errors.password ? 'form-control is-invalid' : 'form-control'}
+              type='password'
+              {...register('password', { required: true })}
+            />
+            {errors.password && <div className='invalid-feedback'>Password is required</div>}
+          </div>
+          <div>
+            <small>
+              Don't have an account? <Link to='/register'>Register</Link> instead
+            </small>
+          </div>
+          <button type='submit' className='mt-3 btn btn-success'>
+            Log in
           </button>
-        </div>
+        </form>
       </div>
     </div>
   );
